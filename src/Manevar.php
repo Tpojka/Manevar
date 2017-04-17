b7cd8c33daed4637677c58578e8694f0d88c1d95
Goran Grbic <tpojka@gmail.com>
2017-04-18 00:44:32 +0200

    Comments set
    Comments set and indentation fixed to be more readable.

--- src/Manevar.php
+++ src/Manevar.php
@@ -1,12 +1,19 @@
 <?php
 
+/**
+ * Manevar class
+ */
+
 namespace Tpojka\Manevar;
+
+/**
+ * @return boolean Shows if loopBack manevar is true
+ */
  
-class Manevar {
- 
-  public function loopBack($bool = true)
-  {
-    return $bool;
-  }
- 
+class Manevar
+{
+    public function loopBack($bool = true)
+    {
+        return $bool;
+    }
 }
